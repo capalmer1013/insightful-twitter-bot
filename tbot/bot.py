6ad8357bde69713b9b65c2696417de83932796a7
Chris Palmer <capalmer1013@gmail.com>
2018-10-20 00:20:31 -0400

    terrible class structure

---
+++ tbot/bot.py
@@ -0,0 +1,15 @@
+class Bot(object):
+    def __init__(self):
+        pass
+
+    def learnText(self, inputText):
+        pass
+
+    def outputText(self, charLen=None):
+        pass
+
+    def storeKnowledgeGraph(self):
+        pass
+
+    def loadKnowledgeGraph(self, knowledgeDict):
+        pass
