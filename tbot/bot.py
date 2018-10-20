723faa58c53e68e8326825a50a096a84e6adb3d8
Chris Palmer <capalmer1013@gmail.com>
2018-10-20 00:37:35 -0400

    progress?

--- tbot/bot.py
+++ tbot/bot.py
@@ -1,15 +1,38 @@
+import random
+
+start= "<START>"
+end = "<END>"
+
+
 class Bot(object):
     def __init__(self):
-        pass
+        self.graph = {start: {}, end: None}
 
     def learnText(self, inputText):
-        pass
+        listOfWords = inputText.split()
+        prev = start
+        for word in listOfWords:
+            if prev in self.graph:
+                if word in self.graph[prev]:
+                    self.graph[prev][word] += 1
+
+                else:
+                    self.graph[prev][word] = 1
+
+            else:
+                self.graph[prev] = {}
+                self.graph[prev][word] = 1
+
 
     def outputText(self, charLen=None):
+        outStr = ""
+        nextState = start
+        while nextState != end:
+            selection = []
         pass
 
-    def storeKnowledgeGraph(self):
-        pass
+    def getKnowledgeGraph(self):
+        return self.graph
 
     def loadKnowledgeGraph(self, knowledgeDict):
         pass
